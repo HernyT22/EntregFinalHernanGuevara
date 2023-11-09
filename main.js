9d499aef8493742d99c9ea7e02df88f44c31eeb8
HGuevara <113396996+HernyT22@users.noreply.github.com>
2023-11-09 02:50:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Definición de la clase Perfil
 class Perfil {
     constructor(nombre, gmail, contraseña, edad, domicilio) {
         this.nombre = nombre;
@@ -13,7 +12,6 @@ class Perfil {
     }
 }
 
-// Función para cargar perfiles desde un archivo JSON
 function cargarPerfiles() {
     return new Promise(async (resolve, reject) => {
         try {
@@ -26,19 +24,16 @@ function cargarPerfiles() {
     });
 }
 
-// Comprobación de si el usuario está logueado al cargar la página
 window.addEventListener('DOMContentLoaded', async (event) => {
     const estaLogueado = localStorage.getItem('logueado');
 
     if (estaLogueado === 'true') {
         try {
-            // El usuario está logueado, obtener los datos del perfil desde el localStorage
             const nombreUsuario = localStorage.getItem('nombreUsuario');
             const correoElectronico = localStorage.getItem('correoElectronico');
             const edad = localStorage.getItem('edad');
             const domicilio = localStorage.getItem('domicilio');
 
-            // Mostrar los datos del perfil
             mostrarPerfil({
                 nombre: nombreUsuario,
                 gmail: correoElectronico,
@@ -46,7 +41,6 @@ window.addEventListener('DOMContentLoaded', async (event) => {
                 domicilio: domicilio
             });
 
-            // Mostrar la calculadora y el botón de cerrar sesión
             document.getElementById('calculator').style.display = 'block';
             document.getElementById('cerrarSesion').style.display = 'block';
 
@@ -56,23 +50,18 @@ window.addEventListener('DOMContentLoaded', async (event) => {
     }
 });
 
-// Agregar un controlador de eventos al botón de inicio de sesión
 const botonIniciarSesión = document.getElementById('iniciar');
 
 botonIniciarSesión.addEventListener('click', async function () {
     try {
-        // Obtener los valores ingresados por el usuario
         const correoElectrónico = document.getElementById('gmail').value;
         const contraseña = document.getElementById('contraseña').value;
 
-        // Cargar perfiles desde el archivo JSON
         const perfiles = await cargarPerfiles();
 
-        // Utilizar find para buscar el perfil correspondiente
         const perfilEncontrado = perfiles.find(profile => profile.gmail === correoElectrónico && profile.contraseña === contraseña);
 
         if (perfilEncontrado) {
-            // Mostrar los datos del perfil y almacenarlos en el localStorage
             mostrarPerfil(perfilEncontrado);
             Swal.fire({
                 title: '¡Bien hecho!',
@@ -81,9 +70,7 @@ botonIniciarSesión.addEventListener('click', async function () {
                 confirmButtonText: 'continuar'
             });
 
-            // Ocultar el formulario de inicio de sesión
             document.getElementById('loginForm').style.display = 'none';
-            // Mostrar la calculadora y el botón de cerrar sesión
             document.getElementById('calculator').style.display = 'block';
             document.getElementById('cerrarSesion').style.display = 'block';
         } else {
@@ -99,11 +86,9 @@ botonIniciarSesión.addEventListener('click', async function () {
     }
 });
 
-// Agregar un controlador de eventos al botón de cerrar sesión
 const botonCerrarSesión = document.getElementById('cerrarSesion');
 
 botonCerrarSesión.addEventListener('click', function () {
-    // Utilizar SweetAlert para confirmar el cierre de sesión
     Swal.fire({
         title: '¿Estás seguro?',
         text: 'Estás a punto de cerrar sesión. ¿Quieres continuar?',
@@ -114,31 +99,25 @@ botonCerrarSesión.addEventListener('click', function () {
         confirmButtonText: 'Sí, cerrar sesión'
     }).then((result) => {
         if (result.isConfirmed) {
-            // Eliminar los datos de sesión del localStorage
             localStorage.clear();
-
-            // Recargar la página actual para simular el cierre de sesión
             location.reload();
         }
     });
 });
 
-// Función para mostrar el perfil en la página y almacenar datos en el localStorage
 function mostrarPerfil(perfil) {
     try {
-        // Almacenar los datos de perfil en el localStorage
         localStorage.setItem('logueado', 'true');
         localStorage.setItem('nombreUsuario', perfil.nombre);
         localStorage.setItem('correoElectronico', perfil.gmail);
         localStorage.setItem('edad', perfil.edad);
         localStorage.setItem('domicilio', perfil.domicilio);
 
-        // Mostrar los datos del perfil en la página
         const contenedorPerfil = document.getElementById('perfilContainer');
 
         const perfilInfo = document.createElement('div');
         perfilInfo.innerHTML = `
-            <h2>Bienvenido ${perfil.nombre}</h2>
+            <h2>Bienvenido/a ${perfil.nombre}</h2>
             <p>Correo Electrónico: ${perfil.gmail}, Edad: ${perfil.edad}, Domicilio: ${perfil.domicilio}</p>
         `;
 
@@ -148,7 +127,6 @@ function mostrarPerfil(perfil) {
     }
 }
 
-//calculadora
 let currentInput = '0';
 let operator = '';
 let previousInput = '0';
@@ -223,5 +201,4 @@ function calculate() {
     updateDisplay();
 }
 
-// Initial display update
 updateDisplay();
